bed13a95026ae8f64c13f1bad08f8d74bb1f6c46
maryanabeloshapkina <marianna.industry@gmail.com>
2025-06-03 17:24:01 +0300

    python project

--- modules/goal_methods.py
+++ modules/goal_methods.py
@@ -9,17 +9,18 @@ def create_goal ():
     body = {
         "name": random_name
     }
-    result = requests.post("https://api.clickup.com/api/v2/team/90151249020/goal", headers=my_headers, json=body)
+    result = requests.post("https://api.clickup.com/api/v2/team/90151250102/goal", headers=my_headers, json=body)
     return result
 
 def get_goals():
-    result = requests.get("https://api.clickup.com/api/v2/team/90151249020/goal", headers=my_headers)
+    result = requests.get("https://api.clickup.com/api/v2/team/90151250102/goal", headers=my_headers)
 
 def get_goal_by_id(goal_id):
     result = requests.delete("https://api.clickup.com/api/v2/goal/" + goal_id, headers=my_headers)
 
 def delete_goal(goal_id):
     result = requests.delete("https://api.clickup.com/api/v2/goal/" + goal_id, headers=my_headers)
+
 
 def update_goal(goal_id):
     random_name_for_update = fake.first_name()
@@ -28,3 +29,6 @@ def update_goal(goal_id):
     }
     result = requests.put("https://api.clickup.com/api/v2/goal/" + goal_id, headers=my_headers, json=body_updated)
     return result
+
+def get_goals_without_token():
+    result = requests.get("https://api.clickup.com/api/v2/team/90151250102/goal")
--- tests/test_goals.py
+++ tests/test_goals.py
@@ -2,7 +2,8 @@ import requests
 from faker import Faker
 from pytest_steps import test_steps
 
-from modules.goal_methods import create_goal, get_goals, delete_goal, get_goal_by_id, update_goal
+from modules.goal_methods import create_goal, get_goals, delete_goal, get_goal_by_id, update_goal, \
+    get_goals_without_token
 
 fake=Faker()
 
@@ -13,7 +14,10 @@ my_headers = {"Authorization": "pk_188624974_05RT3BHQDG55DKCKKSJ786V6X5FLKMJG"}
 def test_get_goals():
     result = create_goal ()
     assert result.status_code == 200
-    # assert result.json()["name"] == body.get("name")
+    goal_data = result.json()["goal"]
+    goal_id = goal_data["id"]
+    goal_name = goal_data["name"]
+    print(f"✅ Goal created: {goal_name} (ID: {goal_id})")
     goal_id = result.json()["goal"]["id"]
     yield
     get_goals()
@@ -28,7 +32,6 @@ def test_get_goals():
 def test_create_goal():
     result = create_goal()
     assert result.status_code == 200
-    # assert result.json()["name"] == body.get("name")
     goal_id = result.json()["goal"]["id"]
     yield
     get_goal_by_id(goal_id)
@@ -42,7 +45,6 @@ def test_create_goal():
 def test_get_goal():
     result = create_goal()
     assert result.status_code == 200
-    # assert result.json()["name"] == body.get("name")
     goal_id = result.json()["goal"]["id"]
     yield
     get_goal_by_id(goal_id)
@@ -74,6 +76,19 @@ def test_delete_goal():
     delete_goal(goal_id)
     assert result.status_code == 200
     yield
-    # get_goal_by_id(goal_id)
-    # assert result.status_code == 404
-    # yield
+
+def test_get_goal_without_token():
+    random_name = fake.first_name()
+    body = {
+        "name": random_name
+    }
+    result = requests.post("https://api.clickup.com/api/v2/team/90151250102/goal", headers=my_headers, json=body)
+    assert result.status_code == 200
+    # assert result.json()["name"] == body.get("name")
+    goal_id = result.json()["goal"]["id"]
+
+    result = requests.get("https://api.clickup.com/api/v2/team/90151250102/goal")
+    assert result.status_code == 400
+
+    result = requests.delete("https://api.clickup.com/api/v2/goal/" + goal_id, headers=my_headers)
+    assert result.status_code == 200
